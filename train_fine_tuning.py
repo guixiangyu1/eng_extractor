a796105e57c4eb525581371b6513487d88f2411c
guixiangyu <guixiangyu1@gmail.com>
2019-04-17 10:34:33 +0800

    init

--- train_fine_tuning.py
+++ train_fine_tuning.py
@@ -16,12 +16,12 @@ def main():
     config.clip             = -5.0 # if negative, no clipping
     config.nepoch_no_imprv  = 10
 
-    # config.dir_model = config.dir_output + "model.finetuning.weights/"
+    config.dir_model = config.dir_output + "model.finetuning.weights/"
     
     # build model
     model = NERModel(config)
     model.build("fine_tuning")
-    model.restore_session(config.dir_model, indicate="fine_tuning")
+    model.restore_session("results/test/model.weights/", indicate="fine_tuning")
 
     # model.restore_session("results/crf/model.weights/") # optional, restore weights
     # model.reinitialize_weights("proj")
@@ -33,6 +33,7 @@ def main():
                          config.processing_tag, config.max_iter)
 
     # train model
+
     model.train(train, dev)
 
 if __name__ == "__main__":
